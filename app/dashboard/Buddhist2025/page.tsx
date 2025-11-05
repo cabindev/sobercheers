a6efb2ba4b0ea55da83ebd0a3040227051892d79
yellowDev <sdn.warehouse@gmail.com>
2025-11-05 11:47:20 +0700

    new database

--- app/dashboard/Buddhist2025/page.tsx
+++ app/dashboard/Buddhist2025/page.tsx
@@ -4,7 +4,7 @@ import DashboardBuddhist from './components/charts/allChartBuddhist';
 
 export const metadata = {
   title: 'Buddhist Lent 2025 Dashboard',
-  description: 'แดชบอร์ดสำหรับติดตามและวิเคราะห์ข้อมูลผู้เข้าร่วมกิจกรรมเข้าพรรษา พ.ศ. 2568',
+  description: 'แดชบอร์ดสำหรับติดตามและวิเคราะห์ข้อมูลผู้เข้าร่วมกิจกรรมเข้าพรรษา พ.ศ.2568',
 };
 
 export default function Buddhist2025DashboardPage() {
